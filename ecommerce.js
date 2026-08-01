2417a8e5ae1df2f7f17c2487c9d28b3e1edc4efc
Vani2730 <vanisenthil140@gmail.com>
2026-08-01 14:53:45 +0530

    Fixed Express and MongoDB errors

--- ecommerce.js
+++ ecommerce.js
@@ -1,67 +1,83 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://vanisenthil140_db_user:S7SWmPs1zdHf2MTB@cluster0.v3mto3v.mongodb.net")  //to connect the DB
-.then(()=>{
+// MongoDB Connection
+mongoose
+  .connect("mongodb+srv://vanisenthil140_db_user:S7SWmPs1zdHf2MTB@cluster0.v3mto3v.mongodb.net")
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
-    console.log("Error connecting to MongoDB",err);
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+// Schema
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  id: Number,
+  price: Number,
+  description: String,
+  image: String,
 });
 
-const ProductSchema=new mongoose.Schema({  //schema is the structure that contain the variable and data types
-    name:String,
-    id:Number,
-    price:Number,
-    description:String,
-    image:String
+// Model
+const Product = mongoose.model("Product", ProductSchema);
+
+// Insert Product
+app.post("/getProducts", async (req, res) => {
+  try {
+    const { name, id, price, description, image } = req.body;
+
+    const newProduct = new Product({
+      name,
+      id,
+      price,
+      description,
+      image,
+    });
+
+    await newProduct.save();
+
+    res.status(201).send(newProduct);
+  } catch (err) {
+    res.status(500).send("Error saving product");
+  }
 });
 
-const products=mongoose.model("Product",ProductSchema) //used to create the model model creates the collection in db.
-  
-app.post("/getProducts",async(req,res)=>{ //to give input
-    const{name,id,price,description,image}=req.body;
-const newProduct=new products({name,price,description,image});
-await newProduct.save();
-res.send(newProduct);
-}
-)
-
-   //its displays the data inside the array
-app.get("/getProducts",async(req,res)=>{
-    try{
-    const products=await Products.find();
+// Get All Products
+app.get("/getProducts", async (req, res) => {
+  try {
+    const products = await Product.find();
     res.send(products);
+  } catch (err) {
+    res.status(500).send("Error retrieving products");
+  }
+});
+
+// Get Product by MongoDB _id
+app.get("/getProducts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).send("Product Not Found");
     }
-    catch(err){
-        res.status(500).send("Error");
-    }
-})
 
-app.get("/getProducts/:id",async(req,res)=>{
-      
-    try{
-        const{id}=req.params;
-    const prod=await User.findByIdAndDelete(id);
-    res.send(prod);
-    if(!prod){
-        console.log("Product Not Found Not Found");
-    }
-    }
-    catch(err){
-        res.status(500).send("Error retrieving user data");
-    }
-})
+    res.send(product);
+  } catch (err) {
+    res.status(500).send("Error retrieving product");
+  }
+});
 
-
-
-
- 
-
-
-app.listen(5000,()=>{
-    console.log("server is running on 5000 port");
-})
+// Server
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
